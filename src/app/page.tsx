d0d4412b551162769669c36dc420379a292257c6
Qewertyy <kh786100@gmail.com>
2023-12-17 15:27:48 +0530

    Refactor page component and add load more button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,42 @@
 "use client";
-import Image from "next/image";
 import Navbar from "./Navbar";
 import { topNews } from "@/lib/fetchNews";
 import { useEffect, useState } from "react";
 import { StoryComponent } from "@/components/page";
-
 export default function Home() {
   const [topStory, setTopStory] = useState([]);
+  const [counts, setCounts] = useState(6);
+  const [loading, setLoading] = useState(false);
   const fetchTStory = async () => {
-    const res = await topNews(6);
+    const res = await topNews(counts);
     setTopStory(res);
-  }
+    setLoading(false);
+  };
   useEffect(() => {
     fetchTStory();
-  }, []);
+  }, [counts]);
+  const handleBtnClick = () => {
+    setLoading(true);
+    setCounts(counts + 3);
+  };
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <>
       <Navbar />
-      <section className="w-full">
-        <StoryComponent newsType="Top Stories" newsData={topStory} />
-      </section>
+      <StoryComponent newsType="Top Stories" newsData={topStory} />
+      <div className="flex justify-center">
+        { loading ? (<button type="button" className="bg-white hover:bg-black text-black hover:text-white font-bold py-2 px-4 rounded" disabled>
+          <svg className="animate-spin h-1 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+          Loading....
+        </button>) :
+
+        (<button
+          className="bg-white hover:bg-black text-black hover:text-white font-bold py-2 px-4 rounded"
+          onClick={handleBtnClick}
+        >
+          Load More
+        </button>)
+        }
+      </div>
     </>
-    //</main>
   );
 }
--- src/components/page.tsx
+++ src/components/page.tsx
@@ -3,13 +3,19 @@ import Link from "next/link";
 
 interface StoryComponentProps {
   newsType: string;
-  newsData: any;
+  newsData: {
+    newsId: string;
+    title: string;
+    content: string;
+    url: string;
+    imageUrl: string;
+  }[];
 }
 
 export function StoryComponent({ newsType, newsData }: StoryComponentProps) {
   const items = newsData.map((item: any,index: number) => {
     return (
-    <section key={index+item.title}>
+    <section key={item.newsId}>
       <div>
         <Image
           alt={item.title}
@@ -24,8 +30,8 @@ export function StoryComponent({ newsType, newsData }: StoryComponentProps) {
         />
       </div>
       <div className="flex flex-col justify-center">
-        <h3 className="text-xl font-bold mb-2">{item.title}</h3>
-        <p className="text-zinc-500 dark:text-zinc-400">{item.content}</p>
+        <h3 className="text-lg font-bold mb-2">{item.title}</h3>
+        <p className="text-zinc-100 text-xs dark:text-zinc-400">{item.content}</p>
         <Link
           className="text-blue-500 hover:text-blue-700 mt-4"
           href={item.url}
@@ -36,7 +42,7 @@ export function StoryComponent({ newsType, newsData }: StoryComponentProps) {
     </section>);
   });
   return (
-    <main className="container mx-auto px-4 md:px-6 py-8">
+    <main className="container mx-auto px-4 md:px-6 py-8 pb-0">
       <section className="mb-8">
         <h2 className="text-2xl font-bold mb-4">{newsType}</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">{items}</div>
